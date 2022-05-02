593af349f48c187a868296d43ac949bae7b39be1
KDH_Notebook <destiny1017@nate.com>
2022-05-02 21:46:52 +0900

    SpringJPA 웹개발 실습1

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() {
+        // given
+        Member member = new Member();
+        member.setUserName("member1");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(member.getId());
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+
+    }
+}
