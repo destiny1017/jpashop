d5865984c3c79c82e39479b1f673ccf530cf9483
KDH_Notebook <destiny1017@nate.com>
2022-05-20 20:47:05 +0900

    SpringJPA 웹개발 실습 DB조회 쿼리파일 추가

---
+++ selectSql.sql
@@ -0,0 +1,8 @@
+-- select all tables
+select * from CATEGORY;
+select * from CATEGORY_ITEM ;
+select * from DELIVERY ;
+select * from ITEM ;
+select * from MEMBER ;
+select * from ORDERS ;
+select * from ORDER_ITEM ;
